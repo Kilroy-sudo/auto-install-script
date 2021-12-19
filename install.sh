2f969b9b213382598f34359b2fefccf35fae31f7
Kilroy-sudo <77757534+Kilroy-sudo@users.noreply.github.com>
2021-12-19 11:22:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
 echo "Installed Airgeddon"
 sudo apt -qq install samdump2 -y
 echo "Installed Samdump2"
-sudo apt -qq install metasploit-framework -y
-echo "Installed Metasploit"
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+chmod 755 msfinstall
+./msfinstall
 sudo apt -qq install netdiscover -y
 echo "Installed Netdiscover"
 sudo apt -qq install nmap -y
