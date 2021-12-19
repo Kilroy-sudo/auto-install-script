94ea4e98c07fff4442091f9ccc632334cad0e220
Kilroy-sudo <77757534+Kilroy-sudo@users.noreply.github.com>
2021-12-19 04:45:25 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt -qq install git -y
+echo "Installed Git"
+sudo apt -qq install aircrack-ng -y
+echo "Installed Aircrack Suite"
+git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
+echo "Installed Airgeddon"
+sudo apt -qq install samdump2 -y
+echo "Installed Samdump2"
+sudo apt -qq install metasploit-framework -y
+echo "Installed Metasploit"
+sudo apt -qq install netdiscover -y
+echo "Installed Netdiscover"
+sudo apt -qq install nmap -y
+echo "Installed Nmap"
+sudo apt -qq install wireshark -y
+echo "Installed Wireshark"
