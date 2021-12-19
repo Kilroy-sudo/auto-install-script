1fcb5ba79954c20690baada4d45830789971b298
Kilroy-sudo <77757534+Kilroy-sudo@users.noreply.github.com>
2021-12-19 13:51:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
 echo "Installed Airgeddon"
 sudo apt -qq install samdump2 -y
 echo "Installed Samdump2"
+sudo apt -qq install hydra -y
+echo "Installed Hydra"
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod 755 msfinstall
 ./msfinstall
